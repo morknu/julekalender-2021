c98178b997e78a634fd4c9e99d7bbbf3f165e9ed
morknu <knutsen.morten@gmail.com>
2021-12-04 15:11:02 +0100

    luke 4

--- luke-4/luke_4.py
+++ luke-4/luke_4.py
@@ -3,7 +3,7 @@
 manuscript = '100 000 000 000 000 000 079'
 number_of_zeros = manuscript.count('0')
 
-x = '6' * (number_of_zeros-1) + "716"
+x = '6' * (number_of_zeros-1) + '716'
 y = '3' * number_of_zeros + '63'
 
 print(f'{x}, {y}')
